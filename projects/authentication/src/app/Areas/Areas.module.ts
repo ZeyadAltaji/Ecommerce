9946045cc65eb8283173ce90c823b4575892dda1
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-19 19:05:57 +0300

    building to user account

--- projects/authentication/src/app/Areas/Areas.module.ts
+++ projects/authentication/src/app/Areas/Areas.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserAccountComponent } from './components/UserAccount/UserAccount.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
-  declarations: []
+  declarations: [
+    UserAccountComponent
+  ]
 })
 export class AreasModule { }
--- projects/authentication/src/app/Areas/components/UserAccount/UserAccount.component.ts
+++ projects/authentication/src/app/Areas/components/UserAccount/UserAccount.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-UserAccount',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountComponent implements OnInit {
 
-  constructor() { }
+  LoginForm !:FormGroup;
+  RegisterForm!:FormGroup;
 
-  ngOnInit() {
+  constructor(private fb:FormBuilder,private router:Router) { }
+
+  ngOnInit():void {
+      this.LoginForm=this.fb.group({
+        FName :[''],
+        LName:[''],
+        Email:['',Validators.required],
+        password :['',Validators.required],
+        passwordComfirm:[''],
+      });
   }
+  NewUserForm() {
+    this.RegisterForm =this.fb.group({
+      FName: [null, Validators.required,Validators.pattern('[a-zA-Z]{1,10}')],
+      LName: [null, Validators.required,Validators.pattern('[a-zA-Z]{1,10}')],
+      Email: [null, [Validators.email, Validators.required],Validators.pattern('[^@]+@[^@]+.[a-zA-Z]{2,10}')],
+      password: [null, [Validators.required, Validators.minLength(2)],Validators.pattern('^((?!.*[s])(?=.*[A-Z])(?=.*d).{8,99})'),],
+      passwordComfirm: [null, Validators.required,Validators.pattern('^((?!.*[s])(?=.*[A-Z])(?=.*d).{8,99})'),],
+    })
+
+  }
+
+
 
 }
