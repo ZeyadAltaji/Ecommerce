411a96b4ca0449506c948c602f56275ee83f4de0
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-19 19:09:47 +0300

    coll modules into parent module

--- projects/authentication/src/app/Areas/Areas.module.ts
+++ projects/authentication/src/app/Areas/Areas.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserAccountComponent } from './components/UserAccount/UserAccount.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/Login/Login.component';
+import { BusinessAccountComponent } from './components/BusinessAccount/BusinessAccount.component';
+import { PaymentPageComponent } from './components/Payment-page/Payment-page.component';
 
 @NgModule({
   imports: [
@@ -9,7 +12,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
   ],
   declarations: [
-    UserAccountComponent
+    UserAccountComponent,
+    LoginComponent,
+    BusinessAccountComponent,
+    PaymentPageComponent
   ]
 })
 export class AreasModule { }
