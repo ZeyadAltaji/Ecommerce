c7c68f463358a6caa64f1ef25cd80ecc27da9fc2
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-19 18:36:42 +0300

    create UserAccount

---
+++ projects/authentication/src/app/Areas/components/UserAccount/UserAccount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-UserAccount',
+  templateUrl: './UserAccount.component.html',
+  styleUrls: ['./UserAccount.component.css']
+})
+export class UserAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
