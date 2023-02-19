ec313da6d321a9ff7fa567a014fa3302cc1e3b70
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-19 18:38:02 +0300

    create Payment-page

---
+++ projects/authentication/src/app/Areas/components/Payment-page/Payment-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Payment-page',
+  templateUrl: './Payment-page.component.html',
+  styleUrls: ['./Payment-page.component.css']
+})
+export class PaymentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
