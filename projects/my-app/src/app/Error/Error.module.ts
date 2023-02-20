063de881a4fe9a7b938f5ddf6a6200a08bf7cfb7
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-20 19:14:29 +0300

    bliuding page error and with html & css & imgae

--- projects/my-app/src/app/Error/Error.module.ts
+++ projects/my-app/src/app/Error/Error.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './components/Error/Error.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [ErrorComponent ],
   exports:[
