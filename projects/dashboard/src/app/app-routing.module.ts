da825fb72b8af0158593bc4bbf4255f7383d2174
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-22 15:31:36 +0300

    add routering for home page dash and buliding home

--- projects/dashboard/src/app/app-routing.module.ts
+++ projects/dashboard/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './shared/components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/dashboard/src/app/shared/shared.module.ts
+++ projects/dashboard/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent
   ]
 })
 export class SharedModule { }
