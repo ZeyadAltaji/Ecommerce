d0267c224af76391942236ca67431ecc2cd1d93e
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 18:46:53 +0300

    add all the module into paernt module

--- projects/dashboard/src/app/page/page.module.ts
+++ projects/dashboard/src/app/page/page.module.ts
@@ -3,20 +3,54 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { EmployeesListComponent } from './components/employees/employees-list/employees-list.component';
+import { NewEmployyesComponent } from './components/employees/New-employyes/New-employyes.component';
+import { ProductsListComponent } from './components/products/components/products-list/products-list.component';
+import { ProductsNewComponent } from './components/products/components/products-New/products-New.component';
+import { EditEmployyesComponent } from './components/employees/edit-employyes/edit-employyes.component';
+import { ProductsEditComponent } from './components/products/components/Products-edit/Products-edit.component';
+import { OrderListComponent } from './components/orders/order-list/order-list.component';
+import { ReportsListComponent } from './components/Reports/Reports-list/Reports-list.component';
+import { MessagesListComponent } from './components/Messages/Messages-list/Messages-list.component';
 
 
 
 @NgModule({
   declarations: [
-    EmployeesListComponent
+    // Employees
+    EmployeesListComponent,
+    NewEmployyesComponent,
+    EditEmployyesComponent,
+    //products
+    ProductsListComponent,
+    ProductsNewComponent,
+    ProductsEditComponent,
+    //order
+    OrderListComponent,
+    //Reports
+    ReportsListComponent,
+    //Messages
+    MessagesListComponent
+
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule
   ],exports:[
-    EmployeesListComponent
-
+    // Employees
+    EmployeesListComponent,
+    NewEmployyesComponent,
+    EditEmployyesComponent,
+    //products
+    ProductsListComponent,
+    ProductsNewComponent,
+    ProductsEditComponent,
+    //order
+    OrderListComponent,
+    //Reports
+    ReportsListComponent,
+    //Messages
+    MessagesListComponent
   ]
 })
 export class PageModule { }
