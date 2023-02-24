4090f609f4443a20f55e244c8f8b16b893c54f78
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 18:45:04 +0300

    building message module

---
+++ projects/dashboard/src/app/page/components/Messages/Messages-list/Messages-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Messages-list',
+  templateUrl: './Messages-list.component.html',
+  styleUrls: ['./Messages-list.component.css']
+})
+export class MessagesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
