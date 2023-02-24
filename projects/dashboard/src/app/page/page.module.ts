6d6d3404ab4352432cf8dcf74a94bd83ccb7e050
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 14:15:34 +0300

    some edit

---
+++ projects/dashboard/src/app/page/page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeesComponent } from './components/employees/employees.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmployeesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PageModule { }
