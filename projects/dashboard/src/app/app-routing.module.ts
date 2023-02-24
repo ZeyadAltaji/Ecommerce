5e07163806c19f5c1ec9e24d78918c9c82b9c556
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 18:47:46 +0300

    linked link with sidebar and from app routring

--- projects/dashboard/src/app/app-routing.module.ts
+++ projects/dashboard/src/app/app-routing.module.ts
@@ -5,6 +5,13 @@ import { MainComponent } from './home/components/main/main.component';
 import { SubPageHomeComponent } from './home/components/sub-page-home/sub-page-home.component';
 import { EmployeesListComponent } from './page/components/employees/employees-list/employees-list.component';
 import { NewEmployyesComponent } from './page/components/employees/New-employyes/New-employyes.component';
+import { EditEmployyesComponent } from './page/components/employees/edit-employyes/edit-employyes.component';
+import { ProductsListComponent } from './page/components/products/components/products-list/products-list.component';
+import { ProductsNewComponent } from './page/components/products/components/products-New/products-New.component';
+import { ProductsEditComponent } from './page/components/products/components/Products-edit/Products-edit.component';
+import { OrderListComponent } from './page/components/orders/order-list/order-list.component';
+import { ReportsListComponent } from './page/components/Reports/Reports-list/Reports-list.component';
+import { MessagesListComponent } from './page/components/Messages/Messages-list/Messages-list.component';
 
 const routes: Routes = [
   {
@@ -20,12 +27,57 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'Dashboard',
+    children: [
+      {
+        path: '',
+        component: MainComponent ,
+      },
+      {
+        path: '', component: SubPageHomeComponent ,outlet:'secondary'
+      }
+    ]
+  },
+  // emp
+  {
     path:'Employyes',
     component:EmployeesListComponent
   },{
     path:'NewEmployyes',
     component:NewEmployyesComponent
+  },
+  {
+    path:'edit-employyes',
+    component:EditEmployyesComponent
+  },
+  // prodcuts
+  {
+    path:'Prodcuts',
+    component:ProductsListComponent
+  },
+  {
+    path:'New-products',
+    component:ProductsNewComponent
+  },
+  {
+    path:'Edit-Products',
+    component:ProductsEditComponent
+  },
+  // order
+  {
+    path:'Order-List',
+    component:OrderListComponent
+  },
+  //  Reports
+  {
+    path:'Reports-list',
+    component:ReportsListComponent
+  },
+  {
+    path:'Messages-list',
+    component:MessagesListComponent
   }
+
 
 ];
 
