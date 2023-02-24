0ec6ca9418a9f46b82279ecb8286fce799ad6db0
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 23:43:27 +0300

    create module Analytics

---
+++ projects/dashboard/src/app/page/components/Analytics/Analytics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Analytics',
+  templateUrl: './Analytics.component.html',
+  styleUrls: ['./Analytics.component.css']
+})
+export class AnalyticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
