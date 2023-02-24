d49be0ba86ff125b1ad8409df8b299e305e8d3fb
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-02-24 18:46:16 +0300

    bluiding prodcuts module

---
+++ projects/dashboard/src/app/page/components/Reports/Reports-list/Reports-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Reports-list',
+  templateUrl: './Reports-list.component.html',
+  styleUrls: ['./Reports-list.component.css']
+})
+export class ReportsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
