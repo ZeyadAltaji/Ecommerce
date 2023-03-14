7bfbbfda9fac2147f43e5ce9fe4ee93089e1e0b3
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-14 15:40:52 +0300

    crete module profile user

---
+++ projects/dashboard/src/app/profile-user/profile-user.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileUserComponent } from './profile-user.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProfileUserComponent]
+})
+export class ProfileUserModule { }
