3ef8ef0275dc4e8926f310ec37e6df623cebf82c
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-14 17:37:51 +0300

    initialization porfile page into app module

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { PageModule } from './page/page.module';
+import { ProfileUserModule } from './profile-user/profile-user.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
     HomeModule,
-    PageModule
+    PageModule,
+    ProfileUserModule
 
   ],
   providers: [],
