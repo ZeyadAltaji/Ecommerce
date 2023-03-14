4372cad8b7cd0a2ae27c62d0a72908ca344a92f1
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-14 14:18:41 +0300

    create and coding categer module

---
+++ projects/dashboard/src/app/page/components/Category/Category-list/Category-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Category-list',
+  templateUrl: './Category-list.component.html',
+  styleUrls: ['./Category-list.component.css']
+})
+export class CategoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
