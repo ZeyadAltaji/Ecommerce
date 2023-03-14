9296ae819900f067c19e9aed322e527904fea46f
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-14 14:40:13 +0300

    create category edit comp

---
+++ projects/dashboard/src/app/page/components/Category/Category-edit/Category-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Category-edit',
+  templateUrl: './Category-edit.component.html',
+  styleUrls: ['./Category-edit.component.css']
+})
+export class CategoryEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
