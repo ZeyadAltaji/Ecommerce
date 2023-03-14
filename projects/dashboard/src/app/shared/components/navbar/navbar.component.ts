7633292e08af8531f6bf9cd84d714f2936899e41
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-03-14 18:33:34 +0300

    add drop down

--- projects/dashboard/src/app/shared/components/navbar/navbar.component.ts
+++ projects/dashboard/src/app/shared/components/navbar/navbar.component.ts
@@ -7,10 +7,22 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class NavbarComponent{
   menustatus:boolean=false;
+  showdropdown: boolean = false;
+
   // @Output()sidebarToggled=new EventEmitter<boolean>();
   logo: string = "assets/images/logo.png";
   sidebarToggle(){
     // this.menustatus=!this.menustatus;
     // this.sidebarToggled.emit(this.menustatus);
   }
+  dropdown() {
+    if (this.showdropdown == false) {
+      this.showdropdown = true;
+
+    }
+    else {
+      this.showdropdown = false;
+    }
+  }
 }
+
