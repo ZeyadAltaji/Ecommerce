1d1dffc99dc30dc4e0b5799f5700c6b648f9ef76
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-03 17:24:41 +0300

    add env into projects

--- projects/authentication/src/app/services/authentication.service.ts
+++ projects/authentication/src/app/services/authentication.service.ts
@@ -1,17 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Environment } from '../Environments/Environment';
+import { UserForLogin } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  constructor() { }
+  baseUrl =Environment.baseUrl;
+  constructor( private http: HttpClient) { }
+  // AuthenticationLogin(user:UserForLogin){
+  //   // return this.http.post(this.baseUrl+'Accounts/Login',user)
+  // }
 
-  authUser(user: any) {
-    let UserArray = [];
-    if (localStorage.getItem('Users')) {
-      UserArray = JSON.parse(localStorage.getItem('Users'));
+  AuthenticationLogin(loginObj:any){
+    return this.http.post<any>(`${this.baseUrl}Accounts/Login`,loginObj)
+  }
+  RegisterUser(Registerobj:any){
+    return this.http.post<any>(`${this.baseUrl}Accounts/register`,Registerobj)
 
+  }
 
-    }
-    return UserArray.find(p => p.userName ===user.userName && p.password === user.password)
-    }}
+}
