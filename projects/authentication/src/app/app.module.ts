78523b35d1378bb3f58b2155b1f896a9c4675102
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-03 17:23:43 +0300

    addtion  AuthenticationService into app module

--- projects/authentication/src/app/app.module.ts
+++ projects/authentication/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AreasModule } from './Areas/Areas.module';
 import { SharedModule } from "../../../my-app/src/app/shared/shared.module";
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationService } from './services/authentication.service';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
-    providers: [],
+    providers: [
+      AuthenticationService
+    ],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         AreasModule,
-        SharedModule
+        SharedModule,
+        HttpClientModule,
+        ReactiveFormsModule
     ]
 })
 export class AppModule { }
