bfdd26e175af6e71986eea2f19648d184ab2afb1
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-03 17:24:12 +0300

    add http Clinet and form module

--- projects/authentication/src/app/Areas/Areas.module.ts
+++ projects/authentication/src/app/Areas/Areas.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserAccountComponent } from './components/UserAccount/UserAccount.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/Login/Login.component';
 import { BusinessAccountComponent } from './components/BusinessAccount/BusinessAccount.component';
 import { PaymentPageComponent } from './components/Payment-page/Payment-page.component';
 import { RouterModule } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule,
+    FormsModule
   ],
+  providers:[
+    AuthenticationService
+  ]
+  ,
   declarations: [
     UserAccountComponent,
     LoginComponent,
     BusinessAccountComponent,
-    PaymentPageComponent
+    PaymentPageComponent,
+
   ]
 })
 export class AreasModule { }
