1ff9a346b82a0bc102e37958653e25b63d0eef42
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-09 01:25:24 +0300

    auth login and reg and bis

--- projects/authentication/src/app/services/authentication.service.ts
+++ projects/authentication/src/app/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Environment } from '../Environments/Environment';
-import { UserForLogin } from '../Models/User';
+import { BusinessAccount, UserForLogin, UserForRegister } from '../Models/User';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +9,13 @@ import { UserForLogin } from '../Models/User';
 export class AuthenticationService {
   baseUrl =Environment.baseUrl;
   constructor( private http: HttpClient) { }
-  // AuthenticationLogin(user:UserForLogin){
-  //   // return this.http.post(this.baseUrl+'Accounts/Login',user)
-  // }
-
   AuthenticationLogin(loginObj:any){
     return this.http.post<any>(`${this.baseUrl}Accounts/Login`,loginObj)
   }
-  RegisterUser(Registerobj:any){
+  RegisterUser(Registerobj:UserForRegister){
     return this.http.post<any>(`${this.baseUrl}Accounts/register`,Registerobj)
-
   }
-
+  BusinessAccount(BusinessAccount:BusinessAccount){
+    return this.http.post<any>(`${this.baseUrl}Accounts/BusinessAccount`,BusinessAccount)
+  }
 }
