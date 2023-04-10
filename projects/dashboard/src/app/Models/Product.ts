b2de69956620b02dfaa35c24d32916401b753a8f
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-11 02:05:28 +0300

    work coding into IProducts

---
+++ projects/dashboard/src/app/Models/Product.ts
@@ -0,0 +1,14 @@
+export interface IProducts {
+  id: number;
+  Title:string;
+  Description?:string;
+  // photos?: Photo[];
+  Price:number;
+  offers:number;
+  new:number;
+  Quantity:number;
+  BrandsId:number;
+  CarId:number;
+  CategoryId:number;
+  UserId:number;
+}
