075c1d7c9bd8ecd9cc046be86c6dfaa4926eec97
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-11 02:09:56 +0300

    create method get all

--- projects/dashboard/src/app/services/Product.service.ts
+++ projects/dashboard/src/app/services/Product.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Environment } from '../Environments/Environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IProducts } from '../Models/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-constructor() { }
+  baseUrl =Environment.baseUrl;
+  constructor( private http: HttpClient) { }
 
+  GetAllProducts():Observable<IProducts[]>{
+    return this.http.get<IProducts[]>(`${this.baseUrl}Product`).pipe()
+
+  }
 }
