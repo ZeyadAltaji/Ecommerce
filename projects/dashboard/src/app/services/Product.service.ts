c3f5ccc49f8db6da1b8834215ba2b79f8960e807
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-11 05:53:48 +0300

    call api with servies

--- projects/dashboard/src/app/services/Product.service.ts
+++ projects/dashboard/src/app/services/Product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Environment } from '../Environments/Environments';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IProducts } from '../Models/Product';
+import { IProducts } from '../Models/IProduct';
+import { Product } from '../Classes/Product';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,12 @@ export class ProductService {
 
   GetAllProducts():Observable<IProducts[]>{
     return this.http.get<IProducts[]>(`${this.baseUrl}Product/AllProduct`).pipe()
+  }
+  GetByIdProducts(id:string){
+    return this.http.get<Product>(`${this.baseUrl}Product/Products/`+id.toString());
 
+  }
+  UpdateProducts(id: number){
+    return this.http.get<Product>(`${this.baseUrl}Product/Products/update/`+id.toString());
   }
 }
