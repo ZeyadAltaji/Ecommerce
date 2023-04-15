c19c01ad8fe239ecc37ef23a22a5416bce6155b0
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-15 23:00:52 +0300

    coding into inter face and class

--- projects/dashboard/src/app/Classes/User.ts
+++ projects/dashboard/src/app/Classes/User.ts
@@ -1,2 +1,24 @@
-export class User {
+import { IUser, IUserPhoto } from "../Models/IUser";
+
+export class User implements IUser {
+  id!: number;
+  userName!: string;
+  frist_Name!: string;
+  last_Name!: string;
+  email!: string;
+  password!: string;
+  comfirmPassword!: string;
+  passwordKey!: string;
+  phone1!: string;
+  phone2!: string;
+  address!: string;
+  role!: number;
+  userCreate!: string;
+  createDate!: Date;
+  userUpdate!: string;
+  updateDate!: Date;
+  isDelete!: boolean;
+  isActive!: boolean;
+  images?: IUserPhoto[];
+
 }
--- projects/dashboard/src/app/Models/IUser.ts
+++ projects/dashboard/src/app/Models/IUser.ts
@@ -1,2 +1,27 @@
 export interface IUser {
+  id: number;
+  userName: string;
+  frist_Name: string;
+  last_Name: string;
+  email: string;
+  password: string;
+  comfirmPassword: string;
+  passwordKey: string;
+  phone1: string;
+  phone2: string;
+  address: string;
+  role: number;
+   userCreate: string;
+  createDate: Date;
+  userUpdate: string;
+  updateDate: Date;
+  isDelete: boolean;
+  isActive: boolean;
+  image?: string;
+
+}
+export interface IUserPhoto {
+  Image_userUrl: string;
+  Public_id: string;
+
 }
