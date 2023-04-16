7f0ff908b58cda29b0deedc900f942ac60d1f320
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-16 03:22:52 +0300

    add all rest api

--- projects/authentication/src/app/services/authentication.service.ts
+++ projects/authentication/src/app/services/authentication.service.ts
@@ -2,12 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Environment } from '../Environments/Environment';
 import { BusinessAccount, UserForLogin, UserForRegister } from '../Models/User';
+import { Observable } from 'rxjs';
+import { IUser } from 'projects/dashboard/src/app/Models/IUser';
+import { User } from 'projects/dashboard/src/app/Classes/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   baseUrl =Environment.baseUrl;
+  ListUser:IUser[]=[];
+
   constructor( private http: HttpClient) { }
   AuthenticationLogin(loginObj:any){
     return this.http.post<any>(`${this.baseUrl}Accounts/Login`,loginObj)
@@ -17,5 +22,18 @@ export class AuthenticationService {
   }
   BusinessAccount(BusinessAccount:BusinessAccount){
     return this.http.post<BusinessAccount>(`${this.baseUrl}Accounts/BusinessAccount`,BusinessAccount).pipe()
+  }
+  GetAllUser():Observable<IUser[]>{
+    return this.http.get<IUser[]>(`${this.baseUrl}Accounts/AllUsers`);
+  }
+  GetByIdModal(id:number){
+    return this.http.get<User>(`${this.baseUrl}Accounts/AllUsers/${id}`.toString()).pipe()
+  }
+  DeleteUser(id:number){
+    return this.http.put<User>(`${this.baseUrl}Accounts/Users/Delete/` + id.toString(), {});
+  }
+  UpdateUser(user:User){
+    return this.http.put<User>(`${this.baseUrl}Accounts/Users/update/`+user.id,user);
+
   }
 }
