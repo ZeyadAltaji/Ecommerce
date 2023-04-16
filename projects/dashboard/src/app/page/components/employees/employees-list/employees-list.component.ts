102f35db5b839cc1bbd78f6812670d4de0dd1a09
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-16 03:23:05 +0300

    done work list

--- projects/dashboard/src/app/page/components/employees/employees-list/employees-list.component.ts
+++ projects/dashboard/src/app/page/components/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthenticationService } from 'projects/authentication/src/app/services/authentication.service';
+import { User } from 'projects/dashboard/src/app/Classes/User';
+import { IUser } from 'projects/dashboard/src/app/Models/IUser';
+import { interval } from 'rxjs';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-employees-list',
@@ -6,10 +11,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees-list.component.css']
 })
 export class EmployeesListComponent implements OnInit {
+  constructor(public userService:AuthenticationService) { }
+  ListUser:IUser[]|undefined;
+  DataUser=new User();
+  selectedEmployee: User|undefined;
+  public mainPhotoUrl: string | null = null;
 
-  constructor() { }
-
-  ngOnInit() {
+   ngOnInit() {
+    interval(1000).subscribe(() => {
+      this.userService.GetAllUser().subscribe(listData=>{
+        this.userService.ListUser=listData as IUser[];
+        },
+      error => {
+        console.log('httperror:');
+        console.log(error);
+      });
+    });
   }
-
+  openModal(id: number) {
+    this.userService.GetByIdModal(id)
+      .subscribe(response => {
+        this.selectedEmployee = response;
+        const modal = document.getElementById('DataUserModal');
+        modal?.classList.add('show');
+        modal?.setAttribute('style', 'display: block; padding-right: 17px;');
+        const modalBackdrop = document.createElement('div');
+        modalBackdrop.classList.add('modal-backdrop', 'fade', 'show');
+        document.body.appendChild(modalBackdrop);
+      });
+  }
+  closeModal() {
+    const modal = document.getElementById('DataUserModal');
+   modal?.classList.remove('show');
+   modal?.setAttribute('style', 'display: none; padding-right: 0;');
+   const modalBackdrop = document.querySelector('.modal-backdrop');
+   modalBackdrop?.parentNode?.removeChild(modalBackdrop);
+ }
+ DeleteMehtods(id: number){
+  swal({
+    title: "Are you sure?",
+    text: "Once deleted, you will not be able to recover this imaginary file!",
+    icon: "error",
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      this.userService.DeleteUser(id).subscribe((response) => {
+        console.log(id);
+        if (response) {
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        }
+      });
+    }
+  });
+ }
+ setImageUrl(imageUrl: string) {
+  this.DataUser.image_userUrl = imageUrl;
+}
 }
