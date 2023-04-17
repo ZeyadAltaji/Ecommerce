f86beedaf9cabb1c25b805a8f7a020248f93229b
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-17 07:04:07 +0300

    done work into New Brand

--- projects/dashboard/src/app/page/components/Brand/Brand-New/Brand-New.component.ts
+++ projects/dashboard/src/app/page/components/Brand/Brand-New/Brand-New.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Brands } from 'projects/dashboard/src/app/Classes/Brands';
+import { BrandsService } from 'projects/dashboard/src/app/services/Brands.service';
+import { SweetAlertService } from 'projects/dashboard/src/app/services/SweetAlert.service';
 
 @Component({
   selector: 'app-Brand-New',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Brand-New.component.css']
 })
 export class BrandNewComponent implements OnInit {
+  NewBrandForm!:FormGroup;
+  Brand=new Brands;
 
-  constructor() { }
+  constructor( private fb: FormBuilder,private router: Router,
+    private brandsService:BrandsService,
+    private sweetAlertService :SweetAlertService
+    ) { }
+    ngOnInit() {
+      this.AddNewBrandForm();
+    }
 
-  ngOnInit() {
-  }
+    OnSubmit(){
+      this.MapBrands();
+      this.brandsService.AddProducts(this.Brand).subscribe(
+        (data) => {
+          this.sweetAlertService.success("Success", "brands added successfully.");
+          this.router.navigate(['/Brand']);
+        },(error)=>{
+          console.log(error);
+        }
+      )
+    }
 
+    AddNewBrandForm() {
+      this.NewBrandForm = this.fb.group({
+        NameBrand: [null, [Validators.required, Validators.pattern('[a-zA-Z]{1,10}')]],
+
+      });
+    }
+    get _NameBrand() {
+      return this.NewBrandForm.controls['NameBrand'] as FormGroup;
+    }
+    MapBrands() {
+      this.Brand.name=this._NameBrand.value;
+    }
 }
