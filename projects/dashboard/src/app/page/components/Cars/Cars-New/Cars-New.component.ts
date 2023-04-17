b613aed255cc0b9afc2b0d4a4e18711c2039f083
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-17 07:51:52 +0300

    done work into new cars

--- projects/dashboard/src/app/page/components/Cars/Cars-New/Cars-New.component.ts
+++ projects/dashboard/src/app/page/components/Cars/Cars-New/Cars-New.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Cars } from 'projects/dashboard/src/app/Classes/Cars';
+import { CarService } from 'projects/dashboard/src/app/services/Car.service';
+import { SweetAlertService } from 'projects/dashboard/src/app/services/SweetAlert.service';
 
 @Component({
   selector: 'app-Cars-New',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Cars-New.component.css']
 })
 export class CarsNewComponent implements OnInit {
-
-  constructor() { }
-
+  NewCarForm!:FormGroup;
+  car=new Cars;
+  constructor( private fb: FormBuilder,private router: Router,
+    private carService:CarService,
+    private sweetAlertService :SweetAlertService
+    ) { }
   ngOnInit() {
-  }
+    this.AddNewCarsForm();
 
+  }
+  OnSubmit(){
+    this.MapCars();
+    this.carService.AddCars(this.car).subscribe(
+      (data) => {
+        this.sweetAlertService.success("Success", "Car added successfully.");
+        this.router.navigate(['/Cars']);
+      },(error)=>{
+        console.log(error);
+      }
+    )
+  }
+  AddNewCarsForm() {
+    this.NewCarForm = this.fb.group({
+      NameCar: [null, [Validators.required, Validators.pattern('[a-zA-Z]{1,10}')]],
+
+    });
+  }
+  MapCars() {
+    throw new Error('Method not implemented.');
+  }
 }
