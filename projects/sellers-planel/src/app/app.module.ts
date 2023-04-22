480985e50a50ae591140e9c889323e7fffd5bc43
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-22 23:31:51 +0300

    add module analytcis and profile intp into app mod

--- projects/sellers-planel/src/app/app.module.ts
+++ projects/sellers-planel/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { ProfileUserModule } from './profile-user/profile-user.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    AnalyticsModule,
+    ProfileUserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
