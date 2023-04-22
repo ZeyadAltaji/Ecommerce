63b0a386264619310ef9d5362c7149df1c1e56d7
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-04-22 22:35:43 +0300

    coding to navbar

--- projects/sellers-planel/src/app/shared/components/Navbar/Navbar.component.ts
+++ projects/sellers-planel/src/app/shared/components/Navbar/Navbar.component.ts
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
-
+  menustatus:boolean=false;
+  showdropdown: boolean = false;
   ngOnInit() {
   }
+  sidebarToggle(){
+    // this.menustatus=!this.menustatus;
+    // this.sidebarToggled.emit(this.menustatus);
+  }
+  dropdown() {
+    if (this.showdropdown == false) {
+      this.showdropdown = true;
 
+    }
+    else {
+      this.showdropdown = false;
+    }
+  }
 }
