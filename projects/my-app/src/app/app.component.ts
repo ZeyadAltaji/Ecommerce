8d008e01f5f11edc7fca82320eb68497be8373da
Zeyad Altaji <ziadaltaji130@gmail.com>
2023-05-02 15:41:33 +0300

    DONE TEST ANS DISPALY data in section home

--- projects/my-app/src/app/app.component.ts
+++ projects/my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingService } from 'projects/dashboard/src/app/services/Setting.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  logoData='';
+  constructor(private settingservice: SettingService) {}
+  ngOnInit() {
+     this.getLogoData();
+  }
+
+  setFavicon() {
+    const link: HTMLLinkElement | null = document.querySelector('link[rel="icon"]');
+    if (link) {
+      link.href = this.logoData;
+    }
+  }
+  getLogoData() {
+    this.settingservice.GetByIDlogo(1).subscribe(data => {
+
+      this.logoData = `http://localhost:4203/assets/image/Setting/Logo/${data.isLogoUrl}`;
+
+   });
+ }
+
+
+
 }
--- projects/my-app/src/app/home/components/section-home/section-home.component.ts
+++ projects/my-app/src/app/home/components/section-home/section-home.component.ts
@@ -1,13 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import {  OwlOptions } from 'ngx-owl-carousel-o';
+import { SiderMain } from 'projects/dashboard/src/app/Classes/SiderMain';
+import { SliderMainService } from 'projects/dashboard/src/app/services/SliderMain.service';
 
 @Component({
   selector: 'app-section-home',
   templateUrl: './section-home.component.html',
   styleUrls: ['./section-home.component.css']
 })
-export class SectionHomeComponent {
+export class SectionHomeComponent implements OnInit {
 
+  constructor(public slidermainService :SliderMainService) {  }
+  Data: SiderMain[] = [];
+  SiderMain = new SiderMain();
+
+  imageMain: any;
+  MainImage='';
+  UrlImage = '';
+
+  ngOnInit(): void {
+this.getMainSlider();
+  }
+  getMainSlider() {
+    this.slidermainService.GetAllSiderMain().subscribe({
+      next: (listData) => {
+
+        if (Array.isArray(listData) && listData.length > 0 && listData[0].imageURl) {
+          this.Data=listData
+
+          this.imageMain = listData[0];
+
+          this.MainImage = `http://localhost:4203/assets/image/Slider/${this.imageMain.imageURl}`;
+          console.log(this.MainImage);
+        }
+        this.SiderMain = listData;
+        console.log(listData)
+        if (listData && listData.imageURl) {
+          this.UrlImage = `http://localhost:4203/assets/image/Slider/${listData.imageURl}`;
+          console.log(this.UrlImage)
+        }
+      }
+    });
+  }
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
